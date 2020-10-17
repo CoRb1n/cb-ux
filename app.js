ab2f840e4d597204dbd6727737971fd35ad811dc
corbin.boonzaier <corbin.boonzaier@googlemail.com>
2020-10-17 10:23:22 +0200

    almost done

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ document.addEventListener('DOMContentLoaded', (e) => {
     splash.classList.add('display-none')
   }, 2000)
 })
+
+const splash3 = document.querySelector('.splash3')
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  setTimeout(() => {
+    splash3.classList.add('display-none')
+  }, 2000)
+})
+
